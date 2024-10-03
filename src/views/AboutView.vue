a9a423d8b1bfe07c1fbac4a69175e023290a182e
Vince <jpinard@bluewin.ch>
2024-10-03 22:26:06 +0200

    save

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,51 @@
-<template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
-</template>
+<script lang="ts">
+import PouchDB from 'pouchdb'
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
+export default {
+  data() {
+    return {
+      datas: [],
+      databaseReference: null
+    }
+  },
+
+  methods: {
+    inc() {},
+
+    initDatabase() {
+      const db = new PouchDB('http://localhost:5984/database')
+      if (db) {
+        this.databaseReference = db
+        this.fetchData()
+        return
+      } else {
+        console.log('invalide')
+      }
+    },
+
+    fetchData() {
+      this.databaseReference.db
+        .allDocs({
+          include_docs: true,
+          attachments: true
+        })
+        .then(function (result) {
+          // handle result
+        })
+        .catch(function (err) {
+          console.log(err)
+        })
+    }
+  },
+
+  mounted() {
+    this.initDatabase()
   }
 }
-</style>
+</script>
+
+<template>
+  <h1>InfraDon2</h1>
+  <p>Counter: {{ datas }}</p>
+  <button type="button" role="button" @click="inc">+1</button>
+</template>
